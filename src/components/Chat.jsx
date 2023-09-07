d8bdc6fbad205d373afbf0b228c0b0eb59a42485
Surya-MERNstack <charusurya17@gmail.com>
2023-09-07 18:48:19 +0530

    update

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -312,6 +312,9 @@
 
 // export default Chat;
 
+
+
+
 import React, { useContext, useEffect, useRef, useState } from "react";
 import Avatar from "./Avatar";
 import Logo from "./Logo";
@@ -332,21 +335,18 @@ const Chat = () => {
   const MessageBoxRef = useRef();
 
   useEffect(() => {
-    connectTows();
+    connectToWebSocket();
   }, []);
 
-  // http://localhost:4000/
-
-  const connectTows = () => {
+  const connectToWebSocket = () => {
     const ws = new WebSocket("wss://chatserver-6fpp.onrender.com/users");
     setWs(ws);
-    // https:
 
     ws.addEventListener("message", handleMessage);
     ws.addEventListener("close", () => {
       setTimeout(() => {
-        console.log("Disconnect trying to reconnect!!!");
-        connectTows();
+        console.log("Disconnected, trying to reconnect...");
+        connectToWebSocket();
       }, 1000);
     });
   };
@@ -361,7 +361,6 @@ const Chat = () => {
 
   const handleMessage = (e) => {
     const messageData = JSON.parse(e.data);
-    // console.log(e, messageData);
     if ("online" in messageData) {
       showOnlinePeople(messageData.online);
     } else if ("text" in messageData) {
@@ -391,11 +390,9 @@ const Chat = () => {
       },
     ]);
     if (file) {
-      if (file) {
-        axios.get("/message/" + selectUser).then((res) => {
-          setMessage(res.data);
-        });
-      }
+      axios.get("/message/" + selectUser).then((res) => {
+        setMessage(res.data);
+      });
     }
   };
 
@@ -427,8 +424,8 @@ const Chat = () => {
     }
   }, [selectUser]);
 
-  const onlinePeopleEXclOurUSer = { ...onlinePeople };
-  delete onlinePeopleEXclOurUSer[id];
+  const onlinePeopleEXclOurUser = { ...onlinePeople };
+  delete onlinePeopleEXclOurUser[id];
 
   const messageWithoutDupes = uniqBy(messages, "_id");
 
@@ -440,7 +437,7 @@ const Chat = () => {
     });
   };
 
-  const sendfile = (e) => {
+  const sendFile = (e) => {
     const files = e.target.files[0];
     const reader = new FileReader();
 
@@ -455,16 +452,16 @@ const Chat = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen">
-      <div className="bg-white flex-grow flex flex-col">
+    <div className="flex flex-col lg:flex-row h-screen">
+      <div className="bg-white w-1/4 lg:w-1/3 p-4 overflow-y-auto">
         <Logo />
-        <div className="flex-grow overflow-y-scroll">
-          {Object.keys(onlinePeopleEXclOurUSer).map((userId) => (
+        <div className="flex-grow">
+          {Object.keys(onlinePeopleEXclOurUser).map((userId) => (
             <ContactDetails
               key={userId}
               id={userId}
               online={true}
-              username={onlinePeopleEXclOurUSer[userId]}
+              username={onlinePeopleEXclOurUser[userId]}
               onClick={() => setSelectUser(userId)}
               selected={userId === selectUser}
             />
@@ -480,13 +477,13 @@ const Chat = () => {
             />
           ))}
         </div>
-        <div className="p-2 text-center flex items-center justify-center">
-          <span className="mr-2 text-sm text-gray-600 flex items-center">
+        <div className="mt-auto">
+          <span className="text-sm text-gray-600 flex items-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
-              className="w-5 h-5"
+              className="w-5 h-5 mr-2"
             >
               <path
                 fillRule="evenodd"
@@ -498,123 +495,121 @@ const Chat = () => {
           </span>
           <button
             onClick={logout}
-            className="text-sm bg-red-400 text-white py-1 px-2 border rounded-md"
+            className="text-sm bg-red-400 text-white py-1 px-2 mt-2 border rounded-md"
           >
             Logout
           </button>
         </div>
       </div>
-      <div className="bg-blue-50 flex-grow flex flex-col">
-        {!!selectUser ? (
-          <div className="flex-grow mb-4">
-            <div className="relative h-full">
-              <div className="absolute top-0 left-0 right-0 bottom-2 overflow-y-scroll">
-                {messageWithoutDupes.map((data) => (
-                  <div
-                    key={data._id}
-                    className={
-                      data.sender == id ? "text-right" : "text-left"
-                    }
-                  >
-                    <div
-                      className={
-                        "inline-block p-2 my-2  rounded-md text-sm  " +
-                        (data.sender === id
-                          ? "bg-blue-500 text-white"
-                          : "bg-white text-gray-500")
-                      }
-                    >
-                      {data.text}
-                      {data.file && (
-                        <div className="flex items-center gap-1">
-                          <a
-                            className="underline flex items-center gap-1 "
-                            href={
-                              "https://chatserver-6fpp.onrender.com/uploads/" +
-                              data.file
-                            }
-                            target="_blank"
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth={1.5}
-                              stroke="currentColor"
-                              className="w-6 h-6"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                              />
-                            </svg>
-                            {data.file}
-                          </a>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                ))}
-                <div ref={MessageBoxRef}></div>
+      <div className="bg-blue-50 w-3/4 lg:w-2/3 p-4">
+        <div className="flex-grow">
+          {!selectUser && (
+            <div className="flex h-full flex-grow items-center justify-center">
+              <div className="text-gray-400 flex">
+                <img src={chatimg} alt="" srcSet="" />
               </div>
             </div>
-          </div>
-        ) : (
-          <div className="flex-grow items-center justify-center">
-            <div className="text-gray-400 flex ">
-              <img src={chatimg} alt="" srcSet="" />
+          )}
+          {!!selectUser && (
+            <div className="mb-4 h-full">
+              <div className="relative h-full">
+                <div className="absolute top-0 left-0 right-0 bottom-2 overflow-y-scroll">
+                  {messageWithoutDupes.map((data) => (
+                    <div
+                      key={data._id}
+                      className={
+                        data.sender == id ? "text-right" : "text-left"
+                      }
+                    >
+                      <div
+                        className={
+                          "inline-block p-2 my-2 rounded-md text-sm " +
+                          (data.sender === id
+                            ? "bg-blue-500 text-white"
+                            : "bg-white text-gray-500")
+                        }
+                      >
+                        {data.text}
+                        {data.file && (
+                          <div className="flex items-center gap-1">
+                            <a
+                              className="underline flex items-center gap-1"
+                              href={
+                                "https://chatserver-6fpp.onrender.com/uploads/" +
+                                data.file
+                              }
+                              target="_blank"
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                                />
+                              </svg>
+                              {data.file}
+                            </a>
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  ))}
+                  <div ref={MessageBoxRef}></div>
+                </div>
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
         {!!selectUser && (
-          <div className="p-2">
-            <form className="flex gap-2" onSubmit={sendMessage}>
-              <input
-                type="text"
-                value={newMessage}
-                onChange={(e) => setNewMessage(e.target.value)}
-                placeholder="Type your message"
-                className="bg-white flex-grow border p-2"
-              />
-              <label className="bg-blue-200 p-2 rounded-sm cursor-pointer border border-blue-300">
-                <input type="file" className="hidden" onChange={sendfile} />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13"
-                  />
-                </svg>
-              </label>
-              <button
-                type="submit"
-                className="bg-blue-500 p-2 text-white rounded-sm"
+          <form className="flex gap-2">
+            <input
+              type="text"
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+              placeholder="Type your message"
+              className="bg-white flex-grow border p-2"
+            />
+            <label className="bg-blue-200 p-2 rounded-sm cursor-pointer border border-blue-300">
+              <input type="file" className="hidden" onChange={sendFile} />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-                  />
-                </svg>
-              </button>
-            </form>
-          </div>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13"
+                />
+              </svg>
+            </label>
+            <button type="submit" className="bg-blue-500 p-2 text-white rounded-sm">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                />
+              </svg>
+            </button>
+          </form>
         )}
       </div>
     </div>
